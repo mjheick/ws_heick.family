fd1716a6e3bd64bc340a230dd9e95792aba2b515
Matthew Heick (ftpadmin) <ftpadmin@mein30sec.com>
2021-09-25 15:28:14 +0000

    family tree "working", mental note future procedures

--- tree.php
+++ tree.php
@@ -1,10 +1,64 @@
 <?php
 /*In order to display this page we need an ID */
-$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
-if ($id == 0) {
+$id = isset($_GET['id']) ? intval($_GET['id']) : null;
+if (is_null($id)) {
 	header("Location: .", 302);
 	die();
 }
+
+/* Get ready for database-related fun */
+require_once("Data.php");
+$db = new Data();
+$link = $db->getLink();
+
+/* We have an ID. We need to find us, parents, children + childrens other parents */
+/* pretty much what exists for the person in the person table */
+$me = null;
+$query = 'SELECT * FROM `person` WHERE `id`=' . $id . ' LIMIT 1';
+$result = mysqli_query($link, $query);
+while ($row = mysqli_fetch_assoc($result)) {
+	$me = $row;
+}
+if (is_null($me)) {
+	header("Location: .", 302);
+	die();
+}
+
+/* 2 or more indexes. mainly gonna use 0 and 1 */
+$parent = [];
+$query = 'SELECT * FROM `parents` LEFT JOIN `person` ON `parent`=`id` WHERE `person`=' . $id . ' LIMIT 2';
+$result = mysqli_query($link, $query);
+while ($row = mysqli_fetch_assoc($result)) {
+	$parent[] = $row;
+}
+
+/* Each "other parent" is the key, and the children that share are the values */
+/* Start by getting all my kids, first born to last born */
+$child = []; /* Kid data storage */
+$kids = []; /* used for imploding later. ha, if we could only implode kids sometimes... */
+$query = 'SELECT * FROM `parents` LEFT JOIN `person` ON `person`=`id` WHERE `parent`=' . $id . ' ORDER BY `dob`';
+$result = mysqli_query($link, $query);
+while ($row = mysqli_fetch_assoc($result)) {
+	$child[$row['id']] = $row;
+	$kids[] = $row['person'];
+}
+/* If we have kids, continue on, else book outta here and start displaying */
+$coparent = [];
+if (count($kids) > 0) {
+	/* Get other parents of above kids. 90% of the time it'll be one person, but sometimes it could be 30 */
+	$query = 'SELECT * FROM `parents` LEFT JOIN `person` ON `parent`=`id` WHERE `person` IN (' . implode(',', $kids) . ') AND NOT (`parent` = ' . $id . ')';
+	$result = mysqli_query($link, $query);
+	while ($row = mysqli_fetch_assoc($result)) {
+		if (!isset($coparent[$row['parent']])) {
+			$coparent[$row['parent']] = [
+				'person' => $row,
+				'kids' => []
+			];
+		}
+		$coparent[$row['parent']]['kids'][] = $row['person'];
+	}
+}
+
 ?><!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -50,14 +104,36 @@ body, input {
 <?php require_once("header.php"); ?>
 			<!-- start of a bootstrap-geared family tree diagram -->
 			<div class="row"><!-- labels -->
-				<div class="col text-center"><small>Father</small></div>
+				<div class="col text-center"><small>Parent</small></div>
 				<div class="col"></div>
-				<div class="col text-center"><small>Mother</small></div>
+				<div class="col text-center"><small>Parent</small></div>
 			</div>
 			<div class="row"><!-- People -->
-				<div class="col text-center border-full">Dad Heick<br />1955 - ?</div>
+				<div class="col text-center border-full"><?php
+if (isset($parent[0])) {
+	echo '<a href="tree.php?id=' . $parent[0]['id'] . '">' . $parent[0]['fullname'] . '</a><br />';
+	$dob = substr($parent[0]['dob'], 0, 4);
+	if ($dob == '0000') { echo '?'; } else { echo $dob; }
+	echo ' - ';
+	$dod = substr($parent[0]['dod'], 0, 4);
+	if ($dob == '0000') { echo '?'; } else { echo $dod; }
+} else {
+	echo '?';
+}
+?></div>
 				<div class="col"></div>
-				<div class="col text-center border-full">Mom Heick<br /> 1954 - ?</div>
+				<div class="col text-center border-full"><?php
+if (isset($parent[1])) {
+	echo '<a href="tree.php?id=' . $parent[1]['id'] . '">' . $parent[1]['fullname'] . '</a><br />';
+	$dob = substr($parent[1]['dob'], 0, 4);
+	if ($dob == '0000') { echo '?'; } else { echo $dob; }
+	echo ' - ';
+	$dod = substr($parent[1]['dod'], 0, 4);
+	if ($dob == '0000') { echo '?'; } else { echo $dod; }
+} else {
+	echo '?';
+}
+?></div>
 			</div>
 			<div class="row"><!-- Spacing & Drawing -->
 				<div class="col-2 border-right">&nbsp;</div>
@@ -75,7 +151,14 @@ body, input {
 						<div class="col"></div>
 					</div>
 				</div>
-				<div class="col text-center border-full">Me Heick<br>1981 - ?</div>
+				<div class="col text-center border-full"><?php
+echo $me['fullname'] . '<br />';
+$dob = substr($me['dob'], 0, 4);
+if ($dob == '0000') { echo '?'; } else { echo $dob; }
+echo ' - ';
+$dod = substr($me['dod'], 0, 4);
+if ($dob == '0000') { echo '?'; } else { echo $dod; }
+?></div>
 				<div class="col"><!-- 2x2 grid that helps draw lines -->
 				<div class="row">
 						<div class="col border-right border-bottom">&nbsp;</div>
@@ -86,14 +169,37 @@ body, input {
 					</div>
 				</div>
 			</div>
-			<!-- if we have kids, lets display them with the complimentary parents -->
+<?php
+/* If we have kids, we display them with the identified parent */
+if (count($kids) > 0) { /* Start, kid/coparent logic */
+	foreach ($coparent as $key => $cop_data)
+	{
+		/* Display is simple: We need a list of kids and a coparent */
+		$display_kids = '';
+		$display_coparent = '';
+		foreach ($cop_data['kids'] as $kid_index) {
+			if (strlen($display_kids) > 0) { $display_kids .= '<br />'; }
+			$display_kids .= '<a href="tree.php?id=' . $kid_index . '">' . $child[$kid_index]['fullname'] . '</a> / ';
+			$dob = substr($child[$kid_index]['dob'], 0, 4);
+			if ($dob == '0000') { $display_kids .= '?'; } else { $display_kids .= $dob; }
+			$display_kids .= ' - ';
+			$dod = substr($child[$kid_index]['dod'], 0, 4);
+			if ($dob == '0000') { $display_kids .= '?'; } else { $display_kids .= $dod; }
+		}
+		$display_coparent .= '<a href="tree.php?id=' . $cop_data['person']['id'] . '">' . $cop_data['person']['fullname'] . '</a><br />';
+		$dob = substr($cop_data['person']['dob'], 0, 4);
+		if ($dob == '0000') { $display_coparent .= '?'; } else { $display_coparent .= $dob; }
+		$display_coparent .= ' - ';
+		$dod = substr($cop_data['person']['dod'], 0, 4);
+		if ($dob == '0000') { $display_coparent .= '?'; } else { $display_coparent .= $dod; }
+?>
 			<div class="row"><!-- draw a connecting line -->
 				<div class="col border-right">&nbsp;</div>
 				<div class="col"></div>
 			</div>
 			<div class="row">
 				<div class="col-3"></div>
-				<div class="col-4 text-center border-full">Kid Heick 1 / 2003-?<br />Kid Heick 2 / 2004-?</div>
+				<div class="col-4 text-center border-full"><?php echo $display_kids; ?></div>
 				<div class="col-1"><!-- nest for a horizontal connecting line -->
 					<div class="row">
 						<div class="col border-bottom">&nbsp;</div>
@@ -102,28 +208,13 @@ body, input {
 						<div class="col"></div>
 					</div>
 				</div>
-				<div class="col-3 text-center border-full">CoParent 1<br />1985 - </div>
+				<div class="col-3 text-center border-full"><?php echo $display_coparent; ?></div>
 				<div class="col-1"></div>
 			</div>
-
-			<div class="row"><!-- draw a connecting line -->
-				<div class="col border-right">&nbsp;</div>
-				<div class="col"></div>
-			</div>
-			<div class="row">
-				<div class="col-3"></div>
-				<div class="col-4 text-center border-full">Kid Heick 3 / 2015-?<br />Kid Heick 4 / 2016-?</div>
-				<div class="col-1"><!-- nest for a horizontal connecting line -->
-					<div class="row">
-						<div class="col border-bottom">&nbsp;</div>
-					</div>
-					<div class="row">
-						<div class="col"></div>
-					</div>
-				</div>
-				<div class="col-3 text-center border-full">CoParent 2<br />1981 - </div>
-				<div class="col-1"></div>
-			</div>
+<?php
+	}
+} /* end, kid/coparent logic */
+?>
 			<hr />
 			<!-- Person information -->
 <?php require_once("footer.php"); ?>
