0def9d5bb5dbe40f63b5d593a99effd1063bcce8
Matthew Heick (ftpadmin) <ftpadmin@mein30sec.com>
2021-09-25 01:21:30 +0000

    fixing data, writing search, and making seed program for basic testing

--- search.php
+++ search.php
@@ -1,6 +1,36 @@
 <?php
+/**
+ * We should get a query called "data".
+ */
+$data = isset($_GET['data']) ? trim($_GET['data']) : null;
+if (is_null($data)) { die(); }
 
-$response = [];
+/* We're ready to give a response */
+header("Content-Type: application/json");
 
-header("Content-Type: application/json ");
+/* Lets do processing for things >= 3 characters */
+if (strlen($data) < 3)
+{
+	$response = [];
+	echo json_encode($response);
+	die();
+}
+
+/* Search our names */
+require_once("Data.php");
+$db = new Data();
+$link = $db->getLink();
+$query = 'SELECT `id`, `fullname`, `dob`, `dod` FROM `person` WHERE `fullname` LIKE "%' . $db->escape($data) . '%" LIMIT 10';
+$result = mysqli_query($link, $query);
+while ($row = mysqli_fetch_assoc($result))
+{
+	$response[] = [
+		'id' => $row['id'],
+		'fullname' => $row['fullname'],
+		'fullname' => $row['fullname'],
+		'fullname' => $row['fullname'],
+	];
+}
+
 echo json_encode($response);
+die();
